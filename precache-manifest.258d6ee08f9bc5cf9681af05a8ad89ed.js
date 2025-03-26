a58b0f94a09d495febf6065ef35f2c8b94802074
cMelrakki <145865207+cMelrakki@users.noreply.github.com>
2025-03-26 10:15:11 -0600

    Add files via upload

---
+++ precache-manifest.258d6ee08f9bc5cf9681af05a8ad89ed.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5e4ccc47c8174142df1",
+    "url": "/test/static/css/main.b5eb2dc7.chunk.css"
+  },
+  {
+    "revision": "d5e4ccc47c8174142df1",
+    "url": "/test/static/js/main.d5e4ccc4.chunk.js"
+  },
+  {
+    "revision": "33abb9b91c1c494ab74b",
+    "url": "/test/static/js/1.33abb9b9.chunk.js"
+  },
+  {
+    "revision": "5ad1e219c88e053e0b1e",
+    "url": "/test/static/js/runtime~main.5ad1e219.js"
+  },
+  {
+    "revision": "4f23c15785c42a6e1cbc92a2bd4638d0",
+    "url": "/test/static/media/ember-webfont.4f23c157.woff"
+  },
+  {
+    "revision": "fcfdc07ee9f7d2c8a613ee66c7b700cf",
+    "url": "/test/static/media/logo.fcfdc07e.png"
+  },
+  {
+    "revision": "6fdb90202ce165fe949071010fa64b64",
+    "url": "/test/static/media/iFont.6fdb9020.png"
+  },
+  {
+    "revision": "a20cb40fc095506c2d150371c89e9832",
+    "url": "/test/index.html"
+  }
+];
